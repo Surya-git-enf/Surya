7ac1ec5fd7dd58e122283721d7273757d6d3926c
Surya <s8296659@gmail.com>
2026-04-29 08:33:53 +0530

    Refactor imports and rename Home function to Page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,18 +1,18 @@
 
-import HeroReveal from "@/components/HeroReveal";
-import CanvasScroll from "@/components/CanvasScroll";
-import SkillsOrbit from "@/components/SkillsOrbit";
-import AppsSineWave from "@/components/AppsSineWave";
-import Footer from "@/components/Footer";
+import HeroReveal    from '@/components/HeroReveal'
+import CanvasScroll  from '@/components/CanvasScroll'
+import SkillsOrbit   from '@/components/SkillsOrbit'
+import AppsSineWave  from '@/components/AppsSineWave'
+import Footer        from '@/components/Footer'
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="bg-black min-h-screen">
+    <main style={{ background: '#000' }}>
       <HeroReveal />
       <CanvasScroll />
       <SkillsOrbit />
       <AppsSineWave />
       <Footer />
     </main>
-  );
+  )
 }
