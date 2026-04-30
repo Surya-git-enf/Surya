ed09cf398569e4cad9f7ab33cb19508e053b01cf
Surya <s8296659@gmail.com>
2026-04-30 09:23:05 +0530

    Rename Page component to Home and update styles

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,14 @@
+
+// app/page.tsx
 import HeroReveal from "@/components/HeroReveal";
 import CanvasScroll from "@/components/CanvasScroll";
 import SkillsOrbit from "@/components/SkillsOrbit";
 import AppsSineWave from "@/components/AppsSineWave";
 import Footer from "@/components/Footer";
 
-export default function Page() {
+export default function Home() {
   return (
-    <main className="relative min-h-screen overflow-x-hidden bg-black text-white">
+    <main className="relative flex flex-col w-full bg-white overflow-hidden">
       <HeroReveal />
       <CanvasScroll />
       <SkillsOrbit />
