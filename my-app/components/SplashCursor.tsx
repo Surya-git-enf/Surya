db786ecdcfb5179e3f2be3a1dc224294ad8990b2
Surya <s8296659@gmail.com>
2026-05-02 09:15:09 +0530

    Update SplashCursor.tsx

--- my-app/components/SplashCursor.tsx
+++ my-app/components/SplashCursor.tsx
@@ -73,7 +73,7 @@ export default function SplashCursor({
   const animationFrameId = useRef<number | null>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
+    const canvas = canvasRef.current!;
     if (!canvas) return;
 
     let isActive = true;
