2fc4b041501597adeb03be169fc156977013aa0e
Surya <s8296659@gmail.com>
2026-05-02 11:00:11 +0530

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,15 @@
 
 "use client";
+import dynamic from "next/dynamic";
 import HeroReveal from "@/components/HeroReveal";
+
+const CanvasScroll = dynamic(() => import("@/components/CanvasScroll"), { ssr: false });
 
 export default function Home() {
   return (
     <main>
       <HeroReveal />
+      <CanvasScroll />
     </main>
   );
 }
