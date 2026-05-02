7830420ccba4d74ae21a4d0cb8636c55d1801ff4
Surya <s8296659@gmail.com>
2026-05-02 10:44:19 +0530

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,32 +3,3 @@
 export default function Home() {
   return <h1 style={{color:"black",padding:"40px"}}>Hello World</h1>;
 }
-import { useEffect } from "react";
-import dynamic from "next/dynamic";
-import Footer from "@/components/Footer";
-
-const CanvasScroll = dynamic(() => import("@/components/CanvasScroll"), { ssr: false });
-const SkillsOrbit  = dynamic(() => import("@/components/SkillsOrbit"),  { ssr: false });
-const AppsSineWave = dynamic(() => import("@/components/AppsSineWave"), { ssr: false });
-
-export default function Home() {
-  useEffect(() => {
-    window.onerror = (msg, src, line, col, err) => {
-      document.body.innerHTML = `<pre style="color:red;padding:20px;font-size:14px">
-ERROR: ${msg}
-File: ${src}
-Line: ${line}
-${err?.stack}
-      </pre>`;
-    };
-  }, []);
-
-  return (
-    <main className="relative flex flex-col w-full bg-white overflow-hidden">
-      <CanvasScroll />
-      <SkillsOrbit />
-      <AppsSineWave />
-      <Footer />
-    </main>
-  );
-}
